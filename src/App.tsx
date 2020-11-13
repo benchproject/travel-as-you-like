abe3bb09150bafd3fd755aaa29af112492704f72
atif <atif.cdi215@gmail.com>
2020-11-13 17:57:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Header from './components/Layout/Header';
+
+// import styles from './App.module.scss';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      {/* <section className={styles.gridTest} >
+        <div className={styles.row}>
+          <div className={styles.col1Of2}>
+            col 1 of 2
+          </div>
+          <div className={styles.col1Of2}>
+            col 1 of 2
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.col1Of3}>
+            col 1 of 3
+          </div>
+          <div className={styles.col1Of3}>
+            col 1 of 3
+          </div>
+          <div className={styles.col1Of3}>
+            col 1 of 3
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.col1Of3}>
+            col 1 of 3
+          </div>
+          <div className={styles.col2Of3}>
+            col 2 of 3
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.col1Of4}>
+            col 1 of 4
+          </div>
+          <div className={styles.col1Of4}>
+            col 1 of 4
+          </div>
+          <div className={styles.col2Of4}>
+            col 2 of 4
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.col1Of4}>
+            col 1 of 4
+          </div>
+          <div className={styles.col3Of4}>
+            col  of 4
+          </div>
+        </div>
+      </section> */}
+    </>
+  );
+}
+
+export default App;
